15523fd6fd560e3a5684e58c400fa69217147f47
Tod Davies <davies.t.o@gmail.com>
2014-09-04 20:40:47 -0400

    updated package-install requirements

--- el-pocket.el
+++ el-pocket.el
@@ -1,16 +1,18 @@
-;;; el-pocket.el --- el-pocket :: emacs -> getpocket.com
+;;; el-pocket.el --- el-pocket :: emacs -> getpocket.com ;; -*- lexical-binding: t -*-
 ;; Author: Tod Davies <davies.t.o@gmail.com>
 ;; Created: 4 Aug 2014
 ;; Version: 20140804.2311
 ;; Url: http://github.com/pterygota/el-pocket
 ;; Keywords: emacs, pocket, bookmarks
-;; Package-requires: ((web "0.4.3"))
+;; Package-requires: ((web "20140827.1225"))
+;; Installation/Setup/Usage
 
-;; Installation/Setup/Usage:
 ;; Put this file in your load-path somewhere and require it.
+;; Or (package-install-file "/path/to/el-pocket.el"), given a 
+;; connection to marmalade.
 ;; Now do an M-x el-pocket-authorize. The first time you do this
-;; you will be directed to the oauth/request page, where you can 
-;; click on authorize. After authorizing, return to emacs and 
+;; you will be directed to the oauth/request page, where you can
+;; click on authorize. After authorizing, return to emacs and
 ;; M-x el-pocket-authorize again. This time you should get an access
 ;; token, and it will be saved to "~/.el-pocket-auth.js".
 ;; Once this is done you should be able to el-pocket-add URLs ...
@@ -19,10 +21,14 @@
 ;; (require 'el-pocket)
 ;; (el-pocket-load-auth)
 ;;
-;; -*- lexical-binding: t -*-
 
-(require 'web)
+(eval-when-compile
+  (require 'web)
+  (require 'json)
+  (require 'cl))
+
 (require 'json)
+(require 'web)
 
 ;;various mouse-eared items
 (setq *el-pocket-oauth-request-url* "https://getpocket.com/v3/oauth/request")
@@ -53,21 +59,21 @@
     (setq *access-token-and-username*)))
 
 ;; the authorization dance:
-;; TODO - make a nice interface for this 
+;; TODO - make a nice interface for this
 ;; TODO - maybe use the oauth or oauth2 package instead?
 (defun el-pocket-authorize ()
   (interactive)
   (progn
     (if *access-token-and-username* t
       (el-pocket-load-auth))
-    (if *access-token-and-username* t 
-      (if *request-token* 
+    (if *access-token-and-username* t
+      (if *request-token*
 	  (el-pocket-get-access-token)
 	(el-pocket-get-request-token)))))
 
 ;; once the request token is a-gotten,
 ;; and you've gone to the oauth/authorize page
-;; and and done that, this will then get the 
+;; and and done that, this will then get the
 ;; all-important access-token, huzzah!
 (defun el-pocket-get-access-token ()
   (progn
@@ -91,7 +97,7 @@
     (display-message-or-buffer
      "access a-gotten!")))
 
-;; we don't have a request token yet, so request 
+;; we don't have a request token yet, so request
 ;; one, then send the user to oauth/authorize for
 ;; to authorize this shiz
 (defun el-pocket-get-request-token ()
@@ -173,7 +179,3 @@
 (provide 'el-pocket)
 
 ;;; el-pocket.el ends here
-
-
-
-
